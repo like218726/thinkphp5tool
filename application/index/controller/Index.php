b39f13b641492e174ca928fefee1f82b2c761c3f
like218726 <like218726@vip.qq.com>
2019-01-11 19:45:48 +0800

    增加Thinkphp5详情页里面Tab列表分页

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -16,11 +16,13 @@ class Index extends Controller
 				'CreateToken'=>['name'=>'创建接口用户Token','url'=>'https://github.com/like218726/thinkphp5toolkit/tree/master/CreateToken'],
 				'GetWechatPublicUserInfo'=>['name'=>'关注微信公众号后通过授权登陆自动获取其信息','url'=>'https://github.com/like218726/thinkphp5toolkit/tree/master/GetWechatPublicUserInfo'],
 				'WechatMassage'=>['name'=>'关注微信公众号后通过授权登陆自动获取其信息','url'=>'https://github.com/like218726/thinkphp5toolkit/tree/master/WechatMassage'],
+				'ThinkphpPaginate'=>['name'=>'Thinkphp5详情页里面Tab列表分页','url'=>'https://github.com/like218726/thinkphp5toolkit/tree/master/WechatMassage'],
 			],
 			'demo'=>[
-				'dir'=>['name'=>'遍历循环目录下的文件','url'=>'https://github.com/like218726/thinkphp5toolkit/tree/master/Addr_select'],
-				'excel_output'=>['name'=>'excel导出','url'=>'https://github.com/like218726/thinkphp5toolkit/tree/master/Excel_output'],
+				'dir'=>['name'=>'遍历循环目录下的文件','url'=>'https://github.com/like218726/thinkphp5toolkit/tree/master/ScanDirlFile'],
+//				'excel_output'=>['name'=>'excel导出','url'=>'https://github.com/like218726/thinkphp5toolkit/tree/master/Excel_output'],
 				'video'=>['name'=>'视频播放','url'=>'https://github.com/like218726/thinkphp5toolkit/tree/master/Video'],
+				'md5'=>['name'=>'MD5在线解密','url'=>'https://github.com/like218726/thinkphp5toolkit/tree/master/Md5'],
 			],
 		];
 		$this->assign("menu",$menu);
